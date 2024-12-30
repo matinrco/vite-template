3de47b27e3c81b4ee54fd1c90367dccb8918af4c
Matin Rezaei <matinrco@gmail.com>
2024-12-30 04:28:07 +0330

    Bump tanstack

--- src/containers/Root/index.tsx
+++ src/containers/Root/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
+import { Route as aboutRoute } from '@/routes/$locale/about/route'
 
 export const Root = () => {
   const { t, i18n } = useTranslation('common')
@@ -9,7 +10,9 @@ export const Root = () => {
       {t('title')}
       <h1>Welcome!</h1>
       <h2>this is root page</h2>
-      <Link to={`/${i18n.language}/about`}>to about page</Link>
+      <Link to={aboutRoute.to} params={{ locale: i18n.language }}>
+        to about page
+      </Link>
     </div>
   )
 }
